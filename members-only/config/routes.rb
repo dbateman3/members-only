29066362ead97d605835bfd106d77c4eb5254f78
Dan Bateman <dbateman0319@starkstate.net>
2017-08-26 22:10:41 -0400

    Added signup and login pages

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'users/new'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
   resources :users
 end
