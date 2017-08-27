1b670771514be0550d23f244f2cd6a40f15461e4
Dan Bateman <dbateman0319@starkstate.net>
2017-08-26 22:58:22 -0400

    Users can now create accounts

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   
 
   root 'static_pages#home'
-
-  get 'static_pages/help'
-
-  get 'users/new'
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  post '/signup', to: 'users#create'
-  resources :users
+  
+  
 end
